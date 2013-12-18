487c739cfe9b461a1801b025f18016b586348f02
Benjamin Hsieh <bhsieh@goodreads.com>
2013-12-18 15:38:19 -0800

    Remove secret key

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,3 +1,4 @@
+#-*- coding: utf-8 -*-#
 # Be sure to restart your server when you modify this file.
 
 # Your secret key is used for verifying the integrity of signed cookies.
@@ -9,4 +10,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Bademporium::Application.config.secret_key_base = '359cf3236b3afe8e6c8770267e0a619c8ea8de930048ccd730127219ee329fee850e6101032e181553776e7f6fbe58752a206f7182bfd791288099e1608ca384'
+Bademporium::Application.config.secret_key_base = Rails.env.development? ? RAILS_SECRET_KEY : ENV['RAILS_SECRET_KEY']
